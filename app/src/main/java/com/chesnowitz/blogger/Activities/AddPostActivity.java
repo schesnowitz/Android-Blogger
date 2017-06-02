f23f585c1883f6b6d1d6fe3d1d2c1dc547aaada9
schesnowitz <steve@chesnowitz.com>
2017-06-02 03:43:55 -0400

    writing to db adding images

--- app/src/main/java/com/chesnowitz/blogger/Activities/AddPostActivity.java
+++ app/src/main/java/com/chesnowitz/blogger/Activities/AddPostActivity.java
@@ -1,6 +1,8 @@
 package com.chesnowitz.blogger.Activities;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,8 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.chesnowitz.blogger.Model.Blog;
 import com.chesnowitz.blogger.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +31,10 @@ public class AddPostActivity extends AppCompatActivity {
   private FirebaseUser user;
   private FirebaseAuth auth;
   private ProgressDialog progress;
+  private Uri imageUri;
+  private static final int GALLERY_CODE = 1;
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +54,19 @@ public class AddPostActivity extends AppCompatActivity {
     postDescription = (EditText) findViewById(R.id.etAddPostDescription);
     buttonSubmit = (Button) findViewById(R.id.bSubmitPost);
 
+
+    // click listener to get image
+
+    postImage.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_CODE); // galery code set as 1 above
+      }
+    });
+
+
     buttonSubmit.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -56,6 +78,17 @@ public class AddPostActivity extends AppCompatActivity {
     });
   }
 
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+      imageUri = data.getData();
+      postImage.setImageURI(imageUri);
+    }
+
+  }
+
   private void startPosting() {
     progress.setMessage("Working on things...");
     progress.show();
@@ -65,6 +98,16 @@ public class AddPostActivity extends AppCompatActivity {
 
     if (!TextUtils.isEmpty(titleValue) && !TextUtils.isEmpty(descriptionValue)) {
     // start uploading content...
+      Blog blog = new Blog("Title", "Description", "imageurl", "timestamp", "user_id");
+
+      databaseReference.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
+        @Override
+        public void onSuccess(Void aVoid) {
+          Toast.makeText(getApplicationContext(), "Item Added", Toast.LENGTH_LONG).show();
+
+          progress.dismiss();
+        }
+      });
     }
   }
 }
